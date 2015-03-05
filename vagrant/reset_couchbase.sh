ac4d1c0b5dd61ef21d277319ab99a7574de41b3c
Kevin Risden <risdenk@avalonconsult.com>
2015-03-05 11:12:29 -0600

    added reset couchbase script

---
+++ vagrant/reset_couchbase.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+/opt/couchbase/bin/couchbase-cli bucket-delete -c 127.0.0.1:8091 --bucket=demo -u couchbase -p couchbase
+
+/opt/couchbase/bin/couchbase-cli xdcr-setup -c 127.0.0.1:8091 --delete --xdcr-cluster-name=elasticsearch -u couchbase -p couchbase
+
+/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=demo --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=796  --bucket-replica=0 -u couchbase -p couchbase
+  
+/opt/couchbase/bin/couchbase-cli xdcr-setup -c 127.0.0.1:8091 --create --xdcr-cluster-name=elasticsearch --xdcr-hostname=elasticsearch.demo:9091 --xdcr-username=couchbase --xdcr-password=couchbase -u couchbase -p couchbase
+  
+/opt/couchbase/bin/couchbase-cli xdcr-replicate -c 127.0.0.1:8091 --create --xdcr-cluster-name=elasticsearch --xdcr-from-bucket=demo --xdcr-to-bucket=demo --xdcr-replication-mode=capi -u couchbase -p couchbase
