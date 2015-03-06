0999a602f9e2f0ed7e8968cbbc1b84a61fc9947b
Kevin Risden <risdenk@avalonconsult.com>
2015-03-06 15:41:40 -0600

    Improved blueprint.py

--- vagrant/blueprint.py
+++ vagrant/blueprint.py
@@ -6,59 +6,87 @@ from os.path import expanduser
 hostname = socket.gethostname()
 home = expanduser("~")
 
-time.sleep(5)
-url = 'http://{0}:8080/api/v1/bootstrap'.format(hostname)
 headers = {'X-Requested-By':'ambari', 'Content-Type':'application/json'}
 
-with open("{0}/.ssh/id_rsa".format(home), "r") as private_key:
-    content = private_key.read()
-
-post_data = {"verbose":True, "sshKey":content, "hosts":[hostname], "user":"vagrant"}
-
-r = requests.post(url, data=json.dumps(post_data), headers=headers, auth=('admin', 'admin'))
-
-if r.status_code != requests.codes.ok:
-    print 'Request failed'
-else:
-    print 'Bootstrapping command successfully sent'
-
-response = ast.literal_eval(r.text)
-requestId = response["requestId"]
-
-print "Waiting on Ambari to install and register host"
-url = "{0}/{1}".format(url, requestId)
-while True:
-    r = requests.get(url, auth=('admin', 'admin'))
-    if "SUCCESS" in r.text:
-        print "Bootstrapping complete"
-        break
-    else:
-        time.sleep(3)
-
-print "Waiting on host to appear"
 url = 'http://{0}:8080/api/v1/hosts'.format(hostname)
-while True:
-    r = requests.get(url, auth=('admin', 'admin'))
-    if "hdp.demo" in r.text:
-        print "Hosts present"
-        break
+r = requests.get(url, auth=('admin', 'admin'))
+
+if hostname not in [host['Hosts']['host_name'] for host in  r.json()['items']]:
+    url = 'http://{0}:8080/api/v1/bootstrap'.format(hostname)
+
+    with open("{0}/.ssh/id_rsa".format(home), "r") as private_key:
+        content = private_key.read()
+
+    post_data = {"verbose":True, "sshKey":content, "hosts":[hostname], "user":"vagrant"}
+
+    r = requests.post(url, data=json.dumps(post_data), headers=headers, auth=('admin', 'admin'))
+
+    if r.status_code != requests.codes.ok:
+        sys.exit("Request failed - " + str(r.status_code) + " - " + r.text)
     else:
-        time.sleep(3)
+        print 'Bootstrapping command successfully sent'
 
-print "Pausing 10 seconds before submitting blueprint to Ambari"
-time.sleep(10)
-print "Submitting blueprint to Ambari"
-url = 'http://{0}:8080/api/v1/blueprints/single-node-cluster'.format(hostname)
-with open("/vagrant/single-node-cluster.json", "r") as blueprint:
-    content = blueprint.read()
+    requestId = r.json()["requestId"]
 
-r = requests.post(url, data=json.dumps(content), headers=headers, auth=('admin', 'admin'))
+    print "Waiting on Ambari to install and register host"
+    url = "{0}/{1}".format(url, requestId)
+    while True:
+        r = requests.get(url, auth=('admin', 'admin'))
+        if "SUCCESS" in r.text:
+            print "Bootstrapping complete"
+            break
+        else:
+            time.sleep(3)
 
-print "Pausing 10 seconds before submitting cluster topology"
-time.sleep(10)
-print 'Submitting cluster topology to Ambari'
-url = 'http://{0}:8080/api/v1/clusters/single-node-cluster'.format(hostname)
-with open("/vagrant/single-node-mapping.json", "r") as cluster:
-    content = cluster.read()
+    print "Waiting on host to appear"
+    url = 'http://{0}:8080/api/v1/hosts'.format(hostname)
+    while True:
+        r = requests.get(url, auth=('admin', 'admin'))
+        if hostname in [host['Hosts']['host_name'] for host in  r.json()['items']]:
+            print "Hosts present"
+            break
+        else:
+            time.sleep(3)
 
-r = requests.post(url, data=json.dumps(content), headers=headers, auth=('admin', 'admin'))
+blueprint_name = 'single-node-cluster'
+
+url = 'http://{0}:8080/api/v1/blueprints/'.format(hostname)
+r = requests.get(url, headers=headers, auth=('admin', 'admin'))
+
+if blueprint_name not in [blueprint['Blueprints']['blueprint_name'] for blueprint in r.json()['items']]:
+    print "Submitting blueprint to Ambari"
+    url = 'http://{0}:8080/api/v1/blueprints/{1}'.format(hostname, blueprint_name)
+    with open("/vagrant/{0}.json".format(blueprint_name), "r") as blueprint:
+        content = blueprint.read()
+
+    r = requests.post(url, data=json.dumps(content), headers=headers, auth=('admin', 'admin'))
+    if r.status_code != requests.codes.created:
+        sys.exit("Submitting blueprint request failed - " + str(r.status_code) + " - " + r.text)
+    else:
+        print 'Submitting blueprint command successfully sent'
+
+mapping_name = 'single-node-mapping'
+
+url = 'http://{0}:8080/api/v1/clusters/'.format(hostname)
+r = requests.get(url, headers=headers, auth=('admin', 'admin'))
+
+if mapping_name not in [cluster['Clusters']['cluster_name'] for cluster in r.json()['items']]:
+    print 'Submitting cluster topology to Ambari'
+    url = 'http://{0}:8080/api/v1/clusters/{1}'.format(hostname, mapping_name)
+    with open("/vagrant/{0}.json".format(mapping_name), "r") as cluster:
+        content = cluster.read()
+
+    r = requests.post(url, data=json.dumps(content), headers=headers, auth=('admin', 'admin'))
+
+    if r.status_code != requests.codes.accepted:
+        sys.exit("Submitting cluster request failed - " + str(r.status_code) + " - " + r.text)
+    else:
+        print 'Submitting cluster command successfully sent'
+        url = r.json()['href']
+        while True:
+            r = requests.get(url, auth=('admin', 'admin'))
+            if r.json()['Requests']['request_status'] != "COMPLETED":
+                print "Cluster installation complete"
+                break
+            else:
+                time.sleep(5)
