87c6213463f49ec67e9553e43c281523fc7167aa
Jordan Moore <moorej@avalonconsult.com>
2015-11-30 11:59:10 -0600

    Use Angular to refresh results in background. Fixes #77.

--- elasticsearch-twitter-webapp/app/search/search.js
+++ elasticsearch-twitter-webapp/app/search/search.js
@@ -4,13 +4,27 @@ angular.module('demo.search', ['ngRoute', 'elasticui'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/search', {
-            templateUrl: 'search/search.html',
-            controller: 'SearchCtrl'
+            templateUrl: 'search/search.html'
         });
     }])
 
-    .controller('SearchCtrl', [function() {
+    .controller('SearchCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 
+        // Function to get the data
+        $scope.getData = function(){
+          $scope.indexVM.refresh();
+        };
+
+        // Function to replicate setInterval using $timeout service.
+        $scope.intervalFunction = function(){
+          $timeout(function() {
+            $scope.getData();
+            $scope.intervalFunction();
+          }, 10*1000);
+        };
+
+        // Kick off the interval
+        $scope.intervalFunction();
     }])
 
     .constant('euiHost', 'http://elasticsearch.demo:9200/') // ACTION: change to cluster address
