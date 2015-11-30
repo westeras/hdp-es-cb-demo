cc2375cf6eff50563c8ae2a59609e932547bf0fc
Kevin Risden <risdenk@avalonconsult.com>
2015-11-30 14:16:56 -0600

    Remove unnecessary pieces of the angular app

--- elasticsearch-twitter-webapp/app/app.js
+++ elasticsearch-twitter-webapp/app/app.js
@@ -2,7 +2,7 @@
 /*global angular:true */
 'use strict';
 
-angular.module('demo', ['ngRoute', 'ui.bootstrap', 'demo.search'])
+angular.module('demo', ['ngRoute', 'demo.search'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/search'});
     }]);
